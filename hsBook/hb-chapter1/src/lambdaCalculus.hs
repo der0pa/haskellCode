9646a40b271a0e91be6fb33cf6b30971bd227e80
der0pa <dsv2xm@gmail.com>
2018-08-28 23:58:44 +0000

    finish second pass at lambda calculus

--- hsBook/hb-chapter1/src/lambdaCalculus.hs
+++ hsBook/hb-chapter1/src/lambdaCalculus.hs
@@ -34,15 +34,66 @@ so
 and dropping the head(arg) \z. reduces to
 zy
 
+
+
+///////////////////////////
+
 Currying is the rule that each lambda can take only one argument at a time.
 
 so
 \xy.xy   is shorthand for
 
-\x.(\y.xy)   this makes the currying extpl
+\x.(\y.xy)   this makes the currying explicit
+
+       
+
+
+///////////////////////////
+
+
+-- 1) (\abc.cba)zz(\wv.w)
+
+--    (\a.\b.\c.cba)(z)z(\w.\v.w)   --explicit currying
+--      *         |__|                 drop \a. head *
+
+--    (\b.\c.cbz)(z)(\w.\v.w)
+--      *     |___|                    drop \b. head
+
+--    (\c.czz)(\w.\v.w)
+--      * |   |_______|                drop \c. head
+
+--    (\w.\v.w)(z)z
+--      *    |__|                      drop \w. head
+
+--    (\v.z)(z)
+--        |__|                         drop \v. head
+
+--        z          evaluates to z
 
 
 
+||||||||||||||||||||||||||||||||||||||
+
+one more example
+
+(𝜆𝑥𝑦𝑧.𝑥𝑧(𝑦𝑧))(𝜆𝑚𝑛.𝑚)(𝜆𝑝.𝑝)
+(𝜆𝑥.𝜆𝑦.𝜆𝑧.𝑥𝑧(𝑦𝑧))(𝜆𝑚.𝜆𝑛.𝑚)(𝜆𝑝.𝑝)     makes  currying  explicit
+  *       *      |____1___|           bind x to first argument 1 
+(𝜆𝑦.𝜆𝑧(𝜆𝑚.𝜆𝑛.𝑚)𝑧(𝑦𝑧))(𝜆𝑝.𝑝)          drop head y.
+  *               *   |_2__|          bind y to next argument 2
+𝜆𝑧(𝜆𝑚.𝜆𝑛.𝑚)(𝑧)((𝜆𝑝.𝑝)𝑧)
+ *           *                       no arguments to apply to z
+    *        *                       dive deeper 
+𝜆𝑧(𝜆𝑛.𝑧)((𝜆𝑝.𝑝)𝑧)                    binding 𝑚 to the argument z
+𝜆𝑛.𝑧                                 unconditionally tosses the argument
+                                     ((𝜆𝑝.𝑝)𝑧)
+                                     and returns
+𝑧
+
+
+
+
+that is all!
 -}
    
 
