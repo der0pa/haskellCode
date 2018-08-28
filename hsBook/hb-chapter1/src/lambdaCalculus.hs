d60af0ee5a745882f9512360318278ba5b479a99
der0pa <dsv2xm@gmail.com>
2018-08-28 18:59:15 +0000

    add some lambda stuff

--- hsBook/hb-chapter1/src/lambdaCalculus.hs
+++ hsBook/hb-chapter1/src/lambdaCalculus.hs
@@ -1,6 +1,6 @@
 {-
 
-'lambda calculus' or more specific 'typed lambda calculus' is the foundation of the Haskell programming language.
+'lambda calculus' or 'typed lambda calculus' is the foundation of the Haskell programming language.
 
 f(x) = x + 1      this function named f takes on argument named x
                   and adds one to it.
@@ -19,12 +19,32 @@ addOne x =
 
 the lambda symbol in haskell is written as a backslash as in:    \
 
-the function has no name - it is a anonymous or unnamed function.
+the lambda function has no name - it is an anonymous or lambda function.
 
 this is what is called the identity function:    \x.x
-the dot separates the 'function head' from the 'function body'
+the dot separates the 'function args' from the 'function body'
+
+so
+(\x.xy)z
+    |__| replace the bound var x with z
 
 
+\z.zy
+
+and dropping the head(arg) \z. reduces to
+zy
+
+Currying is the rule that each lambda can take only one argument at a time.
+
+so
+\xy.xy   is shorthand for
+
+\x.(\y.xy)   this makes the currying extpl
+
+
+
+-}
+   
 
 
 
